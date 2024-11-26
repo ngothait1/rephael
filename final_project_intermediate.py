b203d700a1e5fe5600001a7db73b2391726004ad
ngothait1 <75209361+ngothait1@users.noreply.github.com>
2024-11-26 09:08:43 +0200

    Add files via upload

--- final_project_intermediate.py
+++ final_project_intermediate.py
@@ -1,142 +1,144 @@
 # Python course - level 2 - Intermediate
-# Lesson 40 - Final Project
+# Lesson 40 - Final Project - Revision 1
 # Author: Rephael Sintes
 
 
-def saveNewEntry(last_index_value, ages_sum_value):
-    id_number = input('ID: ')
+def printMenu():
+    print("1. Save a new entry")
+    print("2. Search by ID")
+    print("3. Print ages average")
+    print("4. Print all Names")
+    print("5. Print all IDs")
+    print("6. Print all entries")
+    print("7. Print entry by index")
+    print("8. Exit")
+
+
+def saveNewEntry(entries_lst, ids_dict, ages_sum_value):
+    id_number = input("ID: ")
     if not id_number.isdigit():
-        return {'error': True, 'msg': f'Error: ID must be a number. {id_number} is not a number'}
+        print(f"Error: ID must be a number. {id_number} is not a number")
+        return
     id_number = int(id_number)
-    if id_number in ids_to_indices:
-        existing_entry = entries[ids_to_indices[id_number]]
+    if id_number in ids_dict:
+        existing_entry = entries[ids_dict[id_number]]
         existing_name = f"'name': '{existing_entry[1]}'"
         existing_age = f"'age': {existing_entry[2]}"
-        return {'error': True, 'msg': f'Error: ID already exists: {{{existing_name}, {existing_age}}}'}
+        print(f"Error: ID already exists: {{{existing_name}, {existing_age}}}")
+        return
 
-    name = input('Name: ')
+    name = input("Name: ")
     if not name.isalpha():
-        return {'error': True, 'msg': f'Error: Name must contain only alphabetical characters. {name} is not alphabetical'}
+        print(
+            f"Error: Name must contain only alphabetical characters. {name} is not alphabetical"
+        )
+        return
 
-    age = input('Age: ')
+    age = input("Age: ")
     if not age.isdigit():
-        return {'error': True, 'msg': f'Error: Age must be a whole number. {age} is not a whole number'}
+        print(f"Error: Age must be a whole number. {age} is not a whole number")
+        return
     age = int(age)
 
-    return {'error': False,
-            'id': int(id_number),
-            'name': name,
-            'age': age,
-            'last_index': last_index_value + 1,
-            'ages_sum': ages_sum_value + age
-            }
+    entries_lst.append([id_number, name, age])
+    ids_dict[id_number] = len(entries_lst) - 1
+    print(f"ID [{id_number}] saved successfully")
+    return ages_sum_value + age
 
 
-def searchById():
-    id_to_search = input('Please enter the ID you want to look for: ')
+def searchById(entries_lst):
+    id_to_search = input("Please enter the ID you want to look for: ")
     if not id_to_search.isdigit():
-        return {'error': True, 'msg': f'Error: ID must be a number. {id_to_search} is not a number'}
+        print(f"Error: ID must be a number. {id_to_search} is not a number")
+        return
     id_to_search = int(id_to_search)
     if id_to_search not in ids_to_indices:
-        return {'error': True, 'msg': f'Error: ID {id_to_search} is not saved'}
+        print(f"Error: ID {id_to_search} is not saved")
+        return
     index = ids_to_indices[id_to_search]
-    printByIndex(index)
+    printByIndex(index, entries_lst)
 
 
-def printAgesAverage(last_index_value, ages_sum_value):
-    if last_index_value == -1:
-        return {'error': True, 'msg': 'Error: No ages to calculate. Please enter at least one data entry'}
-    print(ages_sum_value / (last_index_value + 1))
-    return {'error': False}
+def printAgesAverage(entries_num, ages_sum_value):
+    if entries_num == 0:
+        print(0.0)
+    else:
+        print(ages_sum_value / (entries_num))
 
 
 def printAllItems(index_of_item):
     for index, item in enumerate(entries):
-        print(f'{index}. {item[index_of_item]}')
+        print(f"{index}. {item[index_of_item]}")
 
 
 def printAllEntries():
     for index, item in enumerate(entries):
-        print(f'{index}. ID: {item[0]}')
-        print(f'    Name: {item[1]}')
-        print(f'    Age: {item[2]}')
+        print(f"{index}. ID: {item[0]}")
+        print(f"    Name: {item[1]}")
+        print(f"    Age: {item[2]}")
 
 
-def printEntryByIndex(last_index_value):
-    index_to_print = input(
-        'Please enter the index of the entry you want to print: ')
+def printEntryByIndex(last_index, entries_lst):
+    index_to_print = input("Please enter the index of the entry you want to print: ")
     if not index_to_print.isdigit():
-        return {'error': True, 'msg': f'Error: Index must be a number. {index_to_print} is not a number'}
+        print(f"Error: Index must be a number. {index_to_print} is not a number")
+        return
     index_to_print = int(index_to_print)
-    if index_to_print > last_index_value:
-        return {'error': True, 'msg': f'Error: Index out of range. The maximum index allowed is {last_index_value}'}
-    printByIndex(index_to_print)
-    return {'error': False}
+    if index_to_print > last_index:
+        print(f"Error: Index out of range. The maximum index allowed is {last_index}")
+        return
+    printByIndex(index_to_print, entries_lst)
 
 
 def exitProgram():
-    to_exit = input('Are you sure? (y/n)')
-    while to_exit not in ['y', 'n']:
-        to_exit = input('Are you sure? (y/n)')
-    if to_exit == 'y':
+    to_exit = input("Are you sure? (y/n)")
+    while to_exit not in ["y", "n"]:
+        to_exit = input("Are you sure? (y/n)")
+    if to_exit == "y":
         exit(0)
 
 
-def printByIndex(by_index):
-    print(f'ID: {entries[by_index][0]}')
-    print(f'Name: {entries[by_index][1]}')
-    print(f'Age: {entries[by_index][2]}')
+def printByIndex(by_index, entries_lst):
+    print(f"ID: {entries_lst[by_index][0]}")
+    print(f"Name: {entries_lst[by_index][1]}")
+    print(f"Age: {entries_lst[by_index][2]}")
 
 
 entries = []
-ids_to_indices = {}
-last_index = -1
-ages_sum = 0
+ids_to_indices = {101: 0, 102: 1, 103: 2}
+ages_sum = 27 + 36 + 24
+
+entries.append([101, "Rephael", 27])
+entries.append([102, "Nadav", 36])
+entries.append([103, "Sapir", 24])
 
 while True:
-    print('1. Save a new entry')
-    print('2. Search by ID')
-    print('3. Print ages average')
-    print('4. Print all Names')
-    print('5. Print all IDs')
-    print('6. Print all entries')
-    print('7. Print entry by index')
-    print('8. Exit')
-    choice = input('Please enter your choice:')
-
-    if choice not in '12345678' and len(choice) > 1:
-        print(f'Error: Option {choice} does not exist. Please try again')
+    printMenu()
+    option = input("Please enter your option:")
+    if not option.isdigit():
+        print(f"Error: Option must be a whole number. {option} is not a whole number")
         continue
-    elif choice == '1':
-        result = saveNewEntry(last_index, ages_sum)
-        if result['error']:
-            print(result['msg'])
-        else:
-            last_index = result['last_index']
-            ages_sum = result['ages_sum']
-            entries.append([result['id'], result['name'], result['age']])
-            ids_to_indices[result['id']] = last_index
-            print(f'ID [{result['id']}] saved successfully')
-    elif choice == '2':
-        result = searchById()
-        if result['error']:
-            print(result['msg'])
-    elif choice == '3':
-        result = printAgesAverage(last_index, ages_sum)
-        if result['error']:
-            print(result['msg'])
-    elif choice == '4':
+    option = int(option)
+
+    if option < 1 or option > 8:
+        print(f"Error: Option {option} does not exist. Please try again")
+        continue
+    elif option == 1:
+        ages_sum = saveNewEntry(entries, ids_to_indices, ages_sum)
+    elif option == 2:
+        searchById(entries)
+    elif option == 3:
+        printAgesAverage(len(entries), ages_sum)
+    elif option == 4:
         # Prints all names
         printAllItems(index_of_item=1)
-    elif choice == '5':
+    elif option == 5:
         # Prints all IDs
         printAllItems(index_of_item=0)
-    elif choice == '6':
+    elif option == 6:
         printAllEntries()
-    elif choice == '7':
-        result = printEntryByIndex(last_index)
-        if result['error']:
-            print(result['msg'])
-    elif choice == '8':
+    elif option == 7:
+        printEntryByIndex(len(entries) - 1, entries)
+    elif option == 8:
         exitProgram()
-    input('Press enter to continue')
+    input("Press enter to continue")
